c241dc2fae324738cca7dc81dc880807f36298c3
Daria Dzhagaryan <darya_joy@mail.ru>
2021-04-20 16:31:11 +0300

    задание 5

--- lesson5Dzhagaryan.js
+++ lesson5Dzhagaryan.js
@@ -1,34 +1,85 @@
 /* Задание 1. */
-const boardSettings = {
-    rowCount: 8,
-    colCount: 8,
-    evenCells: '#f5f5f5',
-    oddCellsColor: '#080501',
-    startOddCells: 'a1',
-    chessBoard() {
-        const limit = 8;
-        let result = '';
-        for (let i = 0; i< limit; i++) {
-            for (let j = 0; j < limit; j++) {
-                if (i % 2 !== 0) {
-                    result += j % 2 === 0 ? boardSettings.evenCells : boardSettings.oddCellsColor
-                } else {
-                    result += j % 2 === 0 ? boardSettings.oddCellsColor : boardSettings.evenCells
-                }
-            }
-            result += '\n'
-        }
-        return result
-    },
-};
+const chessBoard = document.querySelector('.chessBoard');
 
+let i = 0, count = 0;
 
+while (count < 8 * 8) {
+  let cell = document.createElement('div');
+  chessBoard.appendChild(cell);
 
+  cell.classList.add('cellItem');
 
+  if (i && i % 2)
+    cell.classList.add('cellItemBlack');
 
+  i += ((i + 2) % 9) ? 1 : 2;
+
+  count++;
+}
 
 /* Задание 2. */
 
 const cartItems = {
+    render(item) {
+        return `<div class="item">
+                    <div><b>Название товара</b>: ${item.itemName}</div>
+                    <div><b>Цена за шт.</b>: ${item.itemPrice}</div>
+                    <div><b>Количество</b>: ${item.itemQuantity}</div>
+                    <div><b>Стоимость</b>: ${item.itemQuantity * item.itemPrice}</div>
+                </div>`;
+    },
 
+    
 }
+
+const customBasket = {
+    allItemsDiv: null,
+    clearBtn: null,
+    cartItems,
+    allItems: [
+        {
+            itemId: 11110,
+            itemName: 'Куртка "Adidas"',
+            itemPrice: 6999,
+            itemQuantity: 1,
+        }, 
+        {
+            itemId: 11111,
+            itemName: 'Кеды "Converse"',
+            itemPrice: 7999,
+            itemQuantity: 2,
+    }
+    ],
+    countBasketPrice() {
+        return this.allItems.reduce((totalPrice, cartItem) => 
+        totalPrice + cartItem.itemPrice * cartItem.itemQuantity, 0);
+    },
+    init() {
+        this.allItemsDiv = document.querySelector('.cartList');
+        this.clearBtn = document.querySelector('.clearBtn');
+        this.clearBtn.addEventListener('click', () => this.clearCart());
+        this.render();
+    },
+
+    clearCart() {
+        this.allItems = [];
+        this.render();
+    },
+
+    render() {
+        if (this.allItems.length) {
+            this.allItems.forEach(item => {
+                this.allItemsDiv.insertAdjacentHTML('beforeend', this.cartItems.render(item));
+            });
+            this.allItemsDiv.insertAdjacentHTML('beforeend', `В корзине ${this.allItems.length} позиций(а) стоимостью ${this.countBasketPrice()} руб.`);
+        } else {
+            this.allItemsDiv.textContent = 'Корзина пуста';
+        }
+    },
+};
+
+customBasket.init();
+
+
+
+
